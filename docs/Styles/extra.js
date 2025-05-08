52592c813713e703aebfc667f8166206cd5604aa
Mr-Wood-CS <77886276+Mr-Wood-CS@users.noreply.github.com>
2025-05-08 17:31:42 +0100

    Update Files

---
+++ docs/Styles/extra.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navItems = document.querySelectorAll(".md-nav__item");
+  
+    navItems.forEach(item => {
+      const link = item.querySelector(".md-nav__link");
+  
+      if (link && link.textContent.trim() === "Hidden Tabs") {
+        item.style.display = "none";
+      }
+    });
+  });
